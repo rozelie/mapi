f8d7ff746b418dcfc4a04f5803e4d29cb787a337
rozelie <ryan.ozelie@gmail.com>
2022-06-19 15:37:11 -0500

    use pathlib methods for managing files

--- mapi/persistence.py
+++ mapi/persistence.py
@@ -9,11 +9,9 @@ logger = logging.getLogger(__name__)
 
 def persist_to_pickle(path: Path, data: Any) -> None:
     logger.info(f"Persisting to {path}")
-    with open(path, "wb") as file:
-        pickle.dump(data, file, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(data, path.open("wb"), pickle.HIGHEST_PROTOCOL)
 
 
 def load_pickle(path: Path) -> Any:
     logger.info(f"Loading from {path}")
-    with open(path, "rb") as file:
-        return pickle.load(file)
+    return pickle.load(path.open("rb"))
